f70946ade6fd8217a90393efdf21a609f158d082
Will Johnson <willyum2n@gmail.com>
2021-03-26 15:38:09 -0400

    We are post 'eject' and all setting needed in XCode have been applied. We are pushing apps to the iPad and it is updating live!

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { BleManager, BleError } from "react-native-ble-plx";
 
 export default function App() {
+  const DeviceManager = new BleManager();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "gold",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ ios/bleTest.swift
@@ -0,0 +1,8 @@
+//
+//  bleTest.swift
+//  BlueToothTest
+//
+//  Created by William Johnson on 3/26/21.
+//
+
+import Foundation
